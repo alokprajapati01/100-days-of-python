a177a4bbedbdf52de6a83ca92343117b507a33d5
alokprajapati01 <alokprajapati9553@gmail.com>
2026-04-11 17:39:06 +0530

    Create quiz_brain.py

---
+++ Day-17/quiz_brain.py
@@ -0,0 +1,25 @@
+class QuizBrain:
+    def __init__(self, q_list, ):
+        self.question_number = 0
+        self.score = 0
+        self.questions_list = q_list
+
+    def still_has_questions(self):
+       return self.question_number < len(self.questions_list)
+
+    def next_question(self):
+        curr_question = self.questions_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {curr_question.text} ( True / False ) ? :").lower()
+        self.check_answer(user_answer, curr_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print ("You got it! Right")
+        else:
+            print ("You got it! Wrong")
+        print(f"the correct answer is {correct_answer}")
+        print(f"Your score is {self.score}/{self.question_number}")
+        print("\n")
+
