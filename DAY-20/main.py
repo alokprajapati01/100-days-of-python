7d5901c7f1c143559647abd660963de97ca55a4b
alokprajapati01 <alokprajapati9553@gmail.com>
2026-05-22 10:59:11 +0530

    Update main.py

--- DAY-20/main.py
+++ DAY-20/main.py
@@ -34,11 +34,20 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extand()
         score.increase_score()
 
-    if  snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if  snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         score.game_over()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
 
 
 screen.exitonclick()
