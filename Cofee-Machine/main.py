f7529df0d4b33de995196768752edf2b5bef2873
alokprajapati01 <alokprajapati9553@gmail.com>
2026-05-26 14:33:04 +0530

    Update main.py

--- Cofee-Machine/main.py
+++ Cofee-Machine/main.py
@@ -1,55 +1,25 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-def is_resource_sufficient(order_ingredients):
-    for item in order_ingredients:
-        if order_ingredients[item] > resources[item]:
-            print(f"Sorry there is not enough {item}.")
-            return False
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-    return True
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
 
 is_on = True
 while is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino):")
-    if choice == "off".lower():
+    options = menu.get_items()
+    choice = input(f"What would you like to do? ({options}): ")
+    if choice == "off":
         is_on = False
-    elif choice == "report".lower():
-        print(f"Water:{resources['water']}ml ")
-        print(f"Milk:  {resources['milk']}ml ")
-        print(f"Coffee:  {resources['coffee']}g ")
-        print(f"Money: {profit} ")
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink = MENU[choice]
-        if is_resource_sufficient(drink["ingredients"]):
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
 
 
 
